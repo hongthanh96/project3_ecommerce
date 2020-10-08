d20adb8482546f5a12be07229b4ab6ec02728cfd
hongthanh96 <hongthanh010896@gmail.com>
2020-10-08 10:48:50 +0700

    update coee

---
+++ js/product.js
@@ -0,0 +1,566 @@
+var list_product1 = {
+    page1: [{
+            "image": "./images/product1.jpg",
+            "name": "Bàn làm việc mã BH02",
+            "price": "1.800.000 VND",
+            "sale": "2.200.000 VND"
+        },
+        {
+            "image": "./images/product2.jpg",
+            "name": "Bàn ghế ăn mã XBA183",
+            "price": "21.000.000 VND",
+            "sale": "23.000.000 VND"
+        },
+        {
+            "image": "./images/product3.jpg",
+            "name": "Bộ vỏ chăn ga cotton BH3",
+            "price": "999.000 VND",
+            "sale": "1.100.000 VND"
+        },
+        {
+            "image": "./images/product4.jpg",
+            "name": "Giường Barcelona mã BH4",
+            "price": "3.850.000 VND",
+            "sale": "5.000.000 VND"
+        },
+        {
+            "image": "./images/product5.jpg",
+            "name": "Giá để sách, tài liệu BH5",
+            "price": "1.800.000 VND",
+            "sale": "2.200.000 VND"
+        },
+        {
+            "image": "./images/product1.jpg",
+            "name": "Bàn làm việc mã BH02",
+            "price": "1.800.000 VND",
+            "sale": "2.200.000 VND"
+        },
+        {
+            "image": "./images/product7.jpg",
+            "name": "Bộ vỏ chăn ga cotton BH7",
+            "price": "999.000 VND",
+            "sale": "1.100.000 VND"
+        },
+        {
+            "image": "./images/product2_select2.jpg",
+            "name": "Bàn làm việc mã BH0290",
+            "price": "2.500.000 VND",
+            "sale": "4.000.000 VND"
+        }
+    ],
+    page2: [{
+            "image": "./images/product8.jpg",
+            "name": "Thảm Thổ Nhĩ Kỳ mã BH9",
+            "price": "1.200.000 VND",
+            "sale": "2.000.000 VND"
+        },
+        {
+            "image": "./images/product2_select2.jpg",
+            "name": "Bàn làm việc mã BH0271",
+            "price": "2.500.000 VND",
+            "sale": "4.000.000 VND"
+        },
+        {
+            "image": "./images/product3_select2.jpg",
+            "name": "Sofa bộ Italia mã 9809",
+            "price": "27.500.000 VND",
+            "sale": "40.000.000 VND"
+        },
+        {
+            "image": "./images/product1_select3.jpg",
+            "name": "Sofa bộ Italia mã 9809",
+            "price": "27.500.000 VND",
+            "sale": "40.000.000 VND"
+        },
+        {
+            "image": "./images/product4_select3.jpg",
+            "name": "Sofa bộ Hugo mã BH6907",
+            "price": "22.500.000 VND",
+            "sale": "30.000.000 VND"
+        },
+        {
+            "image": "./images/product1_select2.jpg",
+            "name": "Bàn làm việc mã BH0187",
+            "price": "2.500.000 VND",
+            "sale": "4.000.000 VND"
+        },
+        {
+            "image": "./images/product2_select2.jpg",
+            "name": "Bàn làm việc mã BH0221",
+            "price": "2.500.000 VND",
+            "sale": "4.000.000 VND"
+        },
+        {
+            "image": "./images/product5_select5.jpg",
+            "name": "Bộ 6 tranh waterColor mã BH20",
+            "price": "600.000 VND",
+            "sale": "790.000 VND"
+        }
+    ]
+}
+
+var list_product2 = {
+    page1: [{
+            "image": "./images/product1_select2.jpg",
+            "name": "Bàn làm việc mã BH01",
+            "price": "17.500.000 VND",
+            "sale": "18.500.000 VND"
+        },
+        {
+            "image": "./images/product2_select2.jpg",
+            "name": "Bàn làm việc BH02",
+            "price": "13.500.000 VND",
+            "sale": "14.000.000 VND"
+        },
+        {
+            "image": "./images/product3_select2.jpg",
+            "name": "Sofa bộ Italia 9809",
+            "price": "27.500.000 VND",
+            "sale": "40.000.000 VND"
+        },
+        {
+            "image": "./images/product4_select2.jpg",
+            "name": "Sofa bộ Hugo 6907",
+            "price": "22.500.000 VND",
+            "sale": "30.000.000 VND"
+        }
+
+    ],
+    page2: [{
+            "image": "./images/product5_select2.jpg",
+            "name": "Bàn làm việc mã BH03",
+            "price": "12.500.000 VND",
+            "sale": "14.000.000 VND"
+        },
+        {
+            "image": "./images/product6_select2.jpg",
+            "name": "Bàn làm việc mã BH04",
+            "price": "12.500.000 VND",
+            "sale": "14.000.000 VND"
+        },
+        {
+            "image": "./images/product7_select2.jpg",
+            "name": "Bàn làm việc mã BH05",
+            "price": "12.500.000 VND",
+            "sale": "14.000.000 VND"
+        },
+        {
+            "image": "./images/product8_select2.jpg",
+            "name": "Bàn làm việc mã BH06",
+            "price": "12.500.000 VND",
+            "sale": "14.000.000 VND"
+        }
+    ]
+}
+
+var list_product3 = {
+    page1: [{
+            "image": "./images/product1_select3.jpg",
+            "name": "Giường ngủ Scandinavian",
+            "price": "16.900.000 VND",
+            "sale": "18.000.000 VND"
+        },
+        {
+            "image": "./images/product2_select3.jpg",
+            "name": "Giường ngủ Harvey",
+            "price": "12.500.000 VND",
+            "sale": "14.000.000 VND"
+        },
+        {
+            "image": "./images/product8_select3.jpg",
+            "name": "Giường ngủ Italia 9809",
+            "price": "27.500.000 VND",
+            "sale": "40.000.000 VND"
+        },
+        {
+            "image": "./images/product4_select3.jpg",
+            "name": "Giường ngủ  Bianca 6907",
+            "price": "22.500.000 VND",
+            "sale": "30.000.000 VND"
+        },
+    ],
+    page2: [{
+            "image": "./images/product2_select3.jpg",
+            "name": "Giường ngủ người lớn mã 05",
+            "price": "15.900.000 VND",
+            "sale": "17.000.000 VND"
+        },
+        {
+            "image": "./images/product6_select3.jpg",
+            "name": "Giường ngủ người lớn mã 06",
+            "price": "12.500.000 VND",
+            "sale": "14.000.000 VND"
+        },
+        {
+            "image": "./images/product7_select3.jpg",
+            "name": "Giường ngủ người lớn mã 07",
+            "price": "10.500.000 VND",
+            "sale": "20.000.000 VND"
+        },
+        {
+            "image": "./images/product8_select3.jpg",
+            "name": "Giường ngủ người lớn mã 08",
+            "price": "19.500.000 VND",
+            "sale": "20.000.000 VND"
+        },
+    ]
+}
+
+var list_product4 = {
+    page1: [{
+            "image": "./images/product1_select4.jpg",
+            "name": "Bồn tắm ToTo",
+            "price": "12.000.000 VND"
+        },
+        {
+            "image": "./images/product2_select4.jpg",
+            "name": "Bàn cầu treo tường",
+            "price": "2.500.000 VND"
+        },
+        {
+            "image": "./images/product1_select4.jpg",
+            "name": "Bồn tắm ToTo",
+            "price": "12.000.000 VND"
+        },
+        {
+            "image": "./images/product4_select4.jpg",
+            "name": "Máy nước nóng Panasonic",
+            "price": "2.100.000 VND"
+        }
+    ],
+    page2: [{
+            "image": "./images/product8_select4.jpg",
+            "name": "Bồn cầu Fuco mã 05",
+            "price": "4.500.000 VND"
+        },
+        {
+            "image": "./images/product7_select4.jpg",
+            "name": "Bàn cầu treo tường 06",
+            "price": "3.500.000 VND"
+        },
+        {
+            "image": "./images/product6_select4.jpg",
+            "name": "Bồn tắm ToTo mã 07",
+            "price": "3.200.000 VND"
+        },
+        {
+            "image": "./images/product5_select4.jpg",
+            "name": "Máy nước nóng mã 08",
+            "price": "11.100.000 VND"
+        }
+    ],
+
+};
+
+var list_product5 = {
+    page1: [{
+            "image": "./images/product1_select5.jpg",
+            "name": "Bộ 15 khung tranh treo tường",
+            "price": "650.000 VND",
+            "sale": "890.000 VND"
+        },
+        {
+            "image": "./images/product2_select5.jpg",
+            "name": "Kệ gỗ treo tường tranh ngang",
+            "price": "720.000 VND",
+            "sale": "850.000 VND"
+        },
+        {
+            "image": "./images/product1_select5.jpg",
+            "name": "Bộ 15 khung tranh treo tường",
+            "price": "650.000 VND",
+            "sale": "890.000 VND"
+        },
+        {
+            "image": "./images/product5_select5.jpg",
+            "name": "Bộ 6 tranh waterColor",
+            "price": "600.000 VND",
+            "sale": "790.000 VND"
+        }
+    ],
+    page2: [{
+            "image": "./images/product3_select5.jpg",
+            "name": "Bộ 15 khung tranh mã 05",
+            "price": "350.000 VND",
+            "sale": "590.000 VND"
+        },
+        {
+            "image": "./images/product4_select5.jpg",
+            "name": "Kệ gỗ treo tường mã 06",
+            "price": "620.000 VND",
+            "sale": "850.000 VND"
+        },
+        {
+            "image": "./images/product6_select5.jpg",
+            "name": "Bộ 15 khung tranh mã 07",
+            "price": "850.000 VND",
+            "sale": "990.000 VND"
+        },
+        {
+            "image": "./images/product7_select5.jpg",
+            "name": "Bộ 6 waterColor mã 08",
+            "price": "700.000 VND",
+            "sale": "990.000 VND"
+        }
+    ]
+
+}
+
+
+var page = 1;
+window.onload = function load_products_1() {
+    var data_product1 = change_products1();
+    document.getElementById('list_product1').innerHTML = data_product1;
+    load_products_2();
+    load_products_3();
+    load_products_4();
+    load_products_5();
+}
+
+function change_products1() {
+    var data_product1 = '';
+    if (page > 2) {
+        page = 1
+    }
+    if (page < 1) {
+        page = 2
+    }
+    if (page == 1) {
+        for (let i = 0; i < list_product1.page1.length; i++) {
+            data_product1 += `<div class="des_product"><img src="${list_product1.page1[i].image}" alt="" srcset="">
+            <span class="change_text">${list_product1.page1[i].name}</span>
+            <span class="price">${list_product1.page1[i].price}  <del>${list_product1.page1[i].sale}</del></span>
+            <div class="btn_product">
+                <div class="btn_eye"><button><a href="#"><i class="far fa-eye"></i></a></button></div>
+                <div class="btn_cart"><button><a href="#"><i class="fas fa-shopping-cart"></i></a></button></div>
+            </div></div>`
+        }
+    }
+    if (page == 2) {
+        for (let i = 0; i < list_product1.page2.length; i++) {
+            data_product1 += `<div class="des_product"><img src="${list_product1.page2[i].image}" alt="" srcset="">
+            <span class="change_text">${list_product1.page2[i].name}</span>
+            <span class="price">${list_product1.page2[i].price}  <del>${list_product1.page2[i].sale}</del></span>
+            <div class="btn_product">
+                <div class="btn_eye"><button><a href="#"><i class="far fa-eye"></i></a></button></div>
+                <div class="btn_cart"><button><a href="#"><i class="fas fa-shopping-cart"></i></a></button></div>
+            </div></div>`
+        }
+    }
+    return data_product1;
+}
+
+function change_list_prev() {
+    var data_product1 = change_products1();
+    document.getElementById('list_product1').innerHTML = data_product1;
+    page--;
+}
+
+function change_list_next() {
+    var data_product1 = change_products1();
+    document.getElementById('list_product1').innerHTML = data_product1;
+    page++;
+}
+
+function change_products2() {
+    var data_product2 = '';
+    if (page > 2) {
+        page = 1
+    }
+    if (page < 1) {
+        page = 2
+    }
+    if (page == 1) {
+        for (let i = 0; i < list_product2.page1.length; i++) {
+            data_product2 += `<div class="des_product"><img src="${list_product2.page1[i].image}" alt="" srcset="">
+            <span class="change_text">${list_product2.page1[i].name}</span>
+            <span class="price">${list_product2.page1[i].price}  <del>${list_product2.page1[i].sale}</del></span>
+            <div class="btn_product">
+                <div class="btn_eye"><button><a href="#"><i class="far fa-eye"></i></a></button></div>
+                <div class="btn_cart"><button><a href="#"><i class="fas fa-shopping-cart"></i></a></button></div>
+            </div></div>`
+        }
+    }
+    if (page == 2) {
+        for (let i = 0; i < list_product2.page2.length; i++) {
+            data_product2 += `<div class="des_product"><img src="${list_product2.page2[i].image}" alt="" srcset="">
+            <span class="change_text">${list_product2.page2[i].name}</span>
+            <span class="price">${list_product2.page2[i].price}  <del>${list_product2.page2[i].sale}</del></span>
+            <div class="btn_product">
+                <div class="btn_eye"><button><a href="#"><i class="far fa-eye"></i></a></button></div>
+                <div class="btn_cart"><button><a href="#"><i class="fas fa-shopping-cart"></i></a></button></div>
+            </div></div>`
+        }
+    }
+    return data_product2;
+}
+
+function change_list2_prev() {
+    var data_product1 = change_products2();
+    document.getElementById('list_product2').innerHTML = data_product1;
+    page--;
+}
+
+function change_list2_next() {
+    var data_product1 = change_products2();
+    document.getElementById('list_product2').innerHTML = data_product1;
+    page++;
+}
+
+function load_products_2() {
+    var data_product2 = change_products2();
+    document.getElementById('list_product2').innerHTML = data_product2;
+}
+
+
+function change_products3() {
+    var data_product3 = '';
+    if (page > 2) {
+        page = 1
+    }
+    if (page < 1) {
+        page = 2
+    }
+    if (page == 1) {
+        for (let i = 0; i < list_product3.page1.length; i++) {
+            data_product3 += `<div class="des_product"><img src="${list_product3.page1[i].image}" alt="" srcset="">
+            <span class="change_text">${list_product3.page1[i].name}</span>
+            <span class="price">${list_product3.page1[i].price}  <del>${list_product3.page1[i].sale}</del></span>
+            <div class="btn_product">
+                <div class="btn_eye"><button><a href="#"><i class="far fa-eye"></i></a></button></div>
+                <div class="btn_cart"><button><a href="#"><i class="fas fa-shopping-cart"></i></a></button></div>
+            </div></div>`
+        }
+    }
+    if (page == 2) {
+        for (let i = 0; i < list_product3.page2.length; i++) {
+            data_product3 += `<div class="des_product"><img src="${list_product3.page2[i].image}" alt="" srcset="">
+            <span class="change_text">${list_product3.page2[i].name}</span>
+            <span class="price">${list_product3.page2[i].price}  <del>${list_product3.page2[i].sale}</del></span>
+            <div class="btn_product">
+                <div class="btn_eye"><button><a href="#"><i class="far fa-eye"></i></a></button></div>
+                <div class="btn_cart"><button><a href="#"><i class="fas fa-shopping-cart"></i></a></button></div>
+            </div></div>`
+        }
+    }
+    return data_product3;
+}
+
+function change_list3_prev() {
+    var data_product3 = change_products3();
+    document.getElementById('list_product3').innerHTML = data_product3;
+    page--;
+}
+
+function change_list3_next() {
+    var data_product3 = change_products3();
+    document.getElementById('list_product3').innerHTML = data_product3;
+    page++;
+}
+
+function load_products_3() {
+    var data_product3 = change_products3();
+    document.getElementById('list_product3').innerHTML = data_product3;
+}
+
+
+function change_products4() {
+    var data_product4 = '';
+    if (page > 2) {
+        page = 1
+    }
+    if (page < 1) {
+        page = 2
+    }
+    if (page == 1) {
+        for (let i = 0; i < list_product4.page1.length; i++) {
+            data_product4 += `<div class="des_product"><img src="${list_product4.page1[i].image}" alt="" srcset="">
+            <span class="change_text">${list_product4.page1[i].name}</span>
+            <span class="price">${list_product4.page1[i].price}</span>
+            <div class="btn_product">
+                <div class="btn_eye"><button><a href="#"><i class="far fa-eye"></i></a></button></div>
+                <div class="btn_cart"><button><a href="#"><i class="fas fa-shopping-cart"></i></a></button></div>
+            </div></div>`
+        }
+    }
+    if (page == 2) {
+        for (let i = 0; i < list_product4.page2.length; i++) {
+            data_product4 += `<div class="des_product"><img src="${list_product4.page2[i].image}" alt="" srcset="">
+            <span class="change_text">${list_product4.page2[i].name}</span>
+            <span class="price">${list_product4.page2[i].price}</span>
+            <div class="btn_product">
+                <div class="btn_eye"><button><a href="#"><i class="far fa-eye"></i></a></button></div>
+                <div class="btn_cart"><button><a href="#"><i class="fas fa-shopping-cart"></i></a></button></div>
+            </div></div>`
+        }
+    }
+    return data_product4;
+}
+
+function change_list4_prev() {
+    var data_product4 = change_products4();
+    document.getElementById('list_product4').innerHTML = data_product4;
+    page--;
+}
+
+function change_list4_next() {
+    var data_product4 = change_products4();
+    document.getElementById('list_product4').innerHTML = data_product4;
+    page++;
+}
+
+function load_products_4() {
+    var data_product4 = change_products4();
+    document.getElementById('list_product4').innerHTML = data_product4;
+}
+
+
+function change_products5() {
+    var data_product5 = '';
+    if (page > 2) {
+        page = 1
+    }
+    if (page < 1) {
+        page = 2
+    }
+    if (page == 1) {
+        for (let i = 0; i < list_product5.page1.length; i++) {
+            data_product5 += `<div class="des_product"><img src="${list_product5.page1[i].image}" alt="" srcset="">
+            <span class="change_text">${list_product5.page1[i].name}</span>
+            <span class="price">${list_product5.page1[i].price} <del>${list_product5.page1[i].sale}</del></span>
+            <div class="btn_product">
+                <div class="btn_eye"><button><a href="#"><i class="far fa-eye"></i></a></button></div>
+                <div class="btn_cart"><button><a href="#"><i class="fas fa-shopping-cart"></i></a></button></div>
+            </div></div>`
+        }
+    }
+    if (page == 2) {
+        for (let i = 0; i < list_product5.page2.length; i++) {
+            data_product5 += `<div class="des_product"><img src="${list_product5.page2[i].image}" alt="" srcset="">
+            <span class="change_text">${list_product5.page2[i].name}</span>
+            <span class="price">${list_product5.page2[i].price} <del>${list_product5.page2[i].sale}</del></span>
+            <div class="btn_product">
+                <div class="btn_eye"><button><a href="#"><i class="far fa-eye"></i></a></button></div>
+                <div class="btn_cart"><button><a href="#"><i class="fas fa-shopping-cart"></i></a></button></div>
+            </div></div>`
+        }
+    }
+    return data_product5;
+}
+
+function change_list5_prev() {
+    var data_product5 = change_products5();
+    document.getElementById('list_product5').innerHTML = data_product5;
+    page--;
+}
+
+function change_list5_next() {
+    var data_product5 = change_products5();
+    document.getElementById('list_product5').innerHTML = data_product5;
+    page++;
+}
+
+function load_products_5() {
+    var data_product5 = change_products5();
+    document.getElementById('list_product5').innerHTML = data_product5;
+}
